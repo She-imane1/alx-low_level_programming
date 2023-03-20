1606479141b04a9854cdf36e6faa1765f5ffe143
She-imane1 <imaneashaib9@gmail.com>
2023-03-19 17:29:53 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else
 {
-printf("last digit of %d is %d and is 0\n", n, n % 10);
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
 }
