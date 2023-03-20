9370edbdaecb34b7e564bbeeb85f2e0b3e0c8102
She-imane1 <imaneashaib9@gmail.com>
2023-03-19 19:26:35 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,25 +9,22 @@ int main(void)
 {
 int n;
 int i;
-int h;
 
-for (h = 0 ; h != 8 ; h++)
-{
-for (n = h + 1 ; n != 9 ; n++)
+
+for (n = 0 ; n < 9 ; n++)
 {
 i = n + 1;
 do {
-putchar('0' + h);
 putchar('0' + n);
 putchar('0' + i);
-if (h != 7)
+if (n < 8)
 {
 putchar(',');
 putchar(32);
 }
 i++;
-} while (i != 10);
+} while (i <  10);
 }
-}
+putchar('\n');
 return (0);
 }
