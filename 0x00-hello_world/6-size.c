f1cb9e2a6d1020070768931e1d89d7a297f556ba
She-imane1 <imaneashaib9@gmail.com>
2023-03-19 19:57:51 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int b;
 long int c;
 long long int d;
 float f;
-printf(" size of a char : %lu byte($)\n ", (unsigned long)sizeof(a));
-printf("size of an int : %lu byte($)\n", (unsigned long)sizeof(b));
-printf("size of a long int : %lu byte($)\n", (unsigned long)sizeof(c));
-printf("size of a long long int : %lu byte($)\n", (unsigned long)sizeof(d));
-printf("size of a a float : %lu byte($)\n", (unsigned long)sizeof(f));
+printf("size of a char : %d byte($)\n ", (unsigned long)sizeof(a));
+printf("size of an int : %d byte($)\n", (unsigned long)sizeof(b));
+printf("size of a long int : %d byte($)\n", (unsigned long)sizeof(c));
+printf("size of a long long int : %d byte($)\n", (unsigned long)sizeof(d));
+printf("size of a a float : %d byte($)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
