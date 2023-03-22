ff393a464f41be1685b3663e7259c9a849cee6f5
She-imane1 <imaneashaib9@gmail.com>
2023-03-21 19:31:08 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 /**
  *main - prints _putchar
  *Return: 0 (success)
  */
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+char c = _putchar\n
+while (c)
+{
+_putchar(c);
+c++;
+}
 return (0);
 }
