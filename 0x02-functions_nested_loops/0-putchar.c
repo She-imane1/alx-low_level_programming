35dafb0395e3d984d5455c6567c0be5e3a9df4d2
She-imane1 <imaneashaib9@gmail.com>
2023-03-21 20:10:56 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
- *main - prints _putchar
+ *main - main function
+ *_putchar: prints _putchar
  *Return: 0 (success)
  */
+
 int main(void)
 {
-char c = _putchar\n
-while (c)
-{
-_putchar(c);
-c++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
