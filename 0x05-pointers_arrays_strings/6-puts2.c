fd3829b3bfa2f990e9843ab6a9c71031da05105d
She-imane1 <imaneashaib9@gmail.com>
2023-03-27 19:14:17 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string
+ *@str: the used string reference
+ */
+void puts2(char *str)
+{
+int z = 0;
+while (str[z] != '\0')
+{
+if (z % 2 == 0)
+{
+_putchar(str[z]);
+}
+z++;
+}
+_putchar('\n');
+}
