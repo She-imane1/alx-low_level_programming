be32aa6029becfda517d2a71b4b282c804464c3b
She-imane1 <imaneashaib9@gmail.com>
2023-04-09 23:38:43 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - point
+ *@argc: number
+ *@argv: strings with arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int j;
+for (j = 0; j < argc; j++)
+printf("ùd\n", argv[j]);
+return (0);
+}
