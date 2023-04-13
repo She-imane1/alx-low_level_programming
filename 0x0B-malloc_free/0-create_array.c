f5c1dc291c6f0216380a78441c21738bf4053676
She-imane1 <imaneashaib9@gmail.com>
2023-04-12 17:59:24 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,11 +2,12 @@
 
 /**
  *create_array - creates an array of a spacific size and fill with a char
- *@size : the Size of the array to make
+ *@size: the Size of the array to make
  *@c: characture to fill the array with
  *Return: An array of charactures
  */
 char *create_array(unsigned int size, char c)
+
 {
 unsigned int i;
 char *az;
@@ -22,3 +23,6 @@ while (i < size)
 az[i] = c;
 i++;
 }
+return (az);
+}
+
