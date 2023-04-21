58206d7d85b26af9546cf43a0aa9dda9d0cd313c
She-imane1 <imaneashaib9@gmail.com>
2023-04-20 18:50:59 -0700

    task0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef STDARG_H
+#define STDARG_H
+#include <stdarg.h>
+#endif
+
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+#endif
