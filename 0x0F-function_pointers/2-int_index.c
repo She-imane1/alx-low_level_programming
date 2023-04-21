3ea420213ef3dc2147767670d73604cdd7ce9443
She-imane1 <imaneashaib9@gmail.com>
2023-04-20 22:30:38 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,10 +3,9 @@
 /**
  * int_index - iterate over an array and executes passed functions on the
  * elements.
- * @array: array of intergers.
- * @size: size of array.
- * @cmp: is the fuction to execute on the array.
- *
+ * @array: array of intergers
+ * @size: size of array
+ * @cmp: is the fuction to execute on the array
  * Return: Index (int)
  */
 int int_index(int *array, int size, int (*cmp)(int))
