5b54e07a85bcda540ed741d60e857692d1bf6479
She-imane1 <imaneashaib@gmail.com>
2023-04-27 15:35:01 -0700

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,5 @@
+#include <stdlib.h>
 #include "lists.h"
-
-size_t step_through(const list_t *h)
 
 /**
  *list_len - find the len of a list
@@ -11,24 +10,11 @@ size_t step_through(const list_t *h)
 
 size_t list_len(const list_t *h)
 {
-if (!h)
-return (0);
-/* assuming that since the list isn not null there is */
-/* one else to account for */
-
-if (!h->next)
-return (1);
-return (1 + step_through(h));
-}
-/**
- *step_through - step through the list
- *@h: a pointer to a list
- *Return: a count for every step through the list
- */
-
-size_t step_through(const list_t *h)
+size_t n = 0;
+while (h)
 {
-if (!h->next)
-return (0);
-return (1 + step_through(h->next));
+n++;
+h = h->next;
+}
+return (n);
 }
